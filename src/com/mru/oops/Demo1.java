8f7c87218b36fbe93cfce23044dc58f7a8d3b8f9
manideep77-80 <manideep778010@gmail.com>
2025-01-11 09:51:25 +0530

    DSA

---
+++ src/com/mru/oops/Demo1.java
@@ -0,0 +1,20 @@
+package com.mru.oops;
+
+
+	public class Demo1 {
+		public static void main(String[] args) {
+			Test1 obj1 = new Test1();
+			obj1.addition();
+			
+			int res1 = obj1.subtraction(100, 50);
+			System.out.println("Subtraction...."+res1);
+			obj1.multiplication(100, 50);
+			
+			int result2 = obj1.Division();
+			System.out.println("Division..."+result2);
+			
+			
+		}
+
+	}
+
---
+++ src/com/mru/oops/Interface1.java
@@ -0,0 +1,12 @@
+package com.mru.oops;
+interface Interface1{
+	public void addition();
+	
+	public int subtraction(int num1,int num2);
+	
+	public void multiplication(int num1,int num2);
+	
+	public int Division();
+	
+	
+	}
